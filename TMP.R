0d500f5aca12c382acb1ae9c4bfd190e273ded91
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2024-10-09 16:36:38 -0700

    done to end of 3.4

--- TMP.R
+++ TMP.R
@@ -1,5 +1,12 @@
 library(tidyverse)
 
 
-tmp.df <-
-tmp.df
+abd.bm.tab.src <- read_csv("data/SummaryTables_AbundanceBM.csv") %>% select(-Stock)
+
+table.in <-  abd.bm.tab.src %>% dplyr::filter(Aggregate=="Nass",Scenario == "LTAvg",BM == "Smsy") %>% select(-Aggregate,-Scenario,-BM) %>%
+  mutate_if(is.numeric,function(x){prettyNum(round(x), big.mark=" ")}) %>%
+  dplyr::rename(Étiquette = Label, Moyenne = Mean,	Médiane = Median)
+
+table.in$Étiquette <- str_replace_all(table.in$Étiquette, c( "Sum" = "Somme",  "Agg Fit" ="Ajus Regr", "Sum-Agg" = "Somme - Ajus Regr"))
+
+table.in
