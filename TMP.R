82fa51159a482709060a5ae77eec97e9b2be6742
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2024-10-20 11:20:05 -0700

    started App H

--- TMP.R
+++ TMP.R
@@ -23,3 +23,19 @@ table.in <-  abd.bm.tab.src %>% dplyr::filter(Aggregate=="Nass",Scenario == "LTA
 table.in$Étiquette <- str_replace_all(table.in$Étiquette, c( "Sum" = "Somme",  "Agg Fit" ="Ajus Regr", "Sum-Agg" = "Somme - Ajus Regr"))
 
 table.in
+
+
+
+
+umsy.bm.tab.src <- read_csv("data/SummaryTables_UMSY.csv")
+
+table.in <-  umsy.bm.tab.src %>% dplyr::filter(Aggregate=="SkeenaWild",Scenario == "LTAvg",BM == "Umsy.c") %>%
+  select(-Aggregate,-Scenario,-BM,-Stock) %>%
+  mutate_if(is.numeric,function(x){prettyNum(round(x), big.mark=" ")})
+
+table.in[table.in == "NA"] <- "-"
+
+table.in$Label <- gsub("Agg Fit", "Ajus Regr",  table.in$Label)
+col.names.use <- c("Étiquette","Moyenne","Médiane","p10","p25","p75","p90")
+
+table.in
